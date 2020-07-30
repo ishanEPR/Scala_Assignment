639ce031028613989211911668335a0335bbac3b
ishanEPR <56678017+ishanEPR@users.noreply.github.com>
2020-07-30 15:28:38 +0530

    Function and Data Assignment

--- Assignment5/question2.scala
+++ Assignment5/question2.scala
@@ -1,9 +1,11 @@
 class Rational(n:Int,d:Int)
 {
-        def numer=n
-	      def denum=d
+        def numer=n/gcd(n,d)
+        def denum=d/gcd(n,d)
+        def this(n:Int)=this(n,1)
+        private def gcd(a:Int,b:Int):Int = if(b==0) a else gcd(b,a%b)
         def -(r:Rational)=new Rational(this.numer*r.denum-this.denum*r.numer,this.denum*r.denum)
-	      override def toString=numer+"/"+denum
+	override def toString=numer+"/"+denum
 }
 
 object question2{
